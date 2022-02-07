178dda0f1f033c46c4237a4b59a457d6c1ba9f4f
dev <nathanpaulson54@yahoo.com>
2022-02-07 13:39:52 +0530

    Create stateVariable.sol

---
+++ stateVariable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+ contract stateVariable{
+     bool public a;
+     int public b;
+     uint public c;
+     address public myAddress;
+
+
+
+ function foo()external {
+     uint notestatrVaribale = 456;
+     bool notSataeVariable= false;
+     notestatrVaribale +=7;
+     notSataeVariable=true;
+
+     a=true;
+     b=123;
+     c=500;
+     myAddress = address(1);
+
+
+
+
+ }
+
+
+ }
