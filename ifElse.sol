f082515f2b922ed26cd6c6e2f76a8bff92dfeb75
dev <nathanpaulson54@yahoo.com>
2022-02-07 23:43:27 +0530

    Create ifElse.sol

---
+++ ifElse.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED \
+pragma solidity ^0.8.0;
+
+contract ifElse{
+    function example (uint _x) external pure returns (uint){
+        if (_x<10){
+        return 1;
+        }else if (_x<20){
+        return 2;
+        } else {
+        return 3;
+        }
+    }
+}
