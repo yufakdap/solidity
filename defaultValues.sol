f55651704d01580a6fcfbe9beef53ca516946ec0
dev <nathanpaulson54@yahoo.com>
2022-02-07 23:15:18 +0530

    Create defaultValues.sol

---
+++ defaultValues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract defValue{
+    bool public boo;
+    address public asrd;
+    int public num;
+    uint public anum;
+    bytes32 public byts;
+
+}
