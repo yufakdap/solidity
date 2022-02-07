ebc4f83fce706c06756ad4172d727c4a4eb2a7c3
dev <nathanpaulson54@yahoo.com>
2022-02-07 14:32:04 +0530

    Create globalVariables.sol

---
+++ globalVariables.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract globalVariables{
+    function glovrbls() external view returns (address, uint ,uint)  {
+        address sender= msg.sender;
+        uint timeStamp= block.timestamp;
+        uint blocknum= block.number;
+       // return (sender,timeStamp,blocknum);
+
+    }
+}
