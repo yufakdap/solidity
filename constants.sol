189fce514db3b36f0184ba41dbb5d4e089b31865
dev <nathanpaulson54@yahoo.com>
2022-02-07 23:23:54 +0530

    Create constants.sol

---
+++ constants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity^0.8.0;
+
+contract const{//	21371 gas used// save gas fee if the state variable dosent change, save gas by using a constant function 
+    int public constant MY_NUM = 5000;
+
+}
+contract conste{// 	23471 gas used//
+    int public MY_NUMe =5000;
+}
