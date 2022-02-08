a76f5c605973e9d76d324e87c3706f2869856bd2
dev <nathanpaulson54@yahoo.com>
2022-02-08 22:37:55 +0530

    Create for&While.sol

---
+++ for&While.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+ contract forlop{
+     function loop () external pure {
+         for (uint i=0;i<10;i++){
+             //code
+             if (i==3){
+                 continue;
+             }
+             if (i==5){
+                 break;
+             }
+         uint b=0;
+         while (b<10){
+                 b++;
+             }
+             }
+         }
+     }
+ 
